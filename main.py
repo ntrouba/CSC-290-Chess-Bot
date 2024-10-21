7e65a0a59cef99e03e6d16b2c9006ae351ee7471
Hillary Dunkley <hvgdunkley@gmail.com>
2024-10-21 13:28:30 -0400

    starting prints at beginning of game

--- main.py
+++ main.py
@@ -2,3 +2,8 @@ import chess
 import chess.engine
 import random
 from datetime import datetime
+
+def main(): 
+    print("=====================================================\n              CS 290 Chess Bot Version 0.1\n=====================================================")
+    print("Time: " , datetime.today().strftime('%Y-%m-%d %H:%M:%S'))
+main()
