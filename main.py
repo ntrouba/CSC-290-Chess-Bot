181756bc489642f57eed459e2ee627d3928fb4b6
Hillary Dunkley <hvgdunkley@gmail.com>
2024-10-21 13:37:29 -0400

    more code

--- main.py
+++ main.py
@@ -4,6 +4,11 @@ import random
 from datetime import datetime
 
 def main(): 
+    board = chess.Board()
+    print(board)
     print("=====================================================\n              CS 290 Chess Bot Version 0.1\n=====================================================")
     print("Time: " , datetime.today().strftime('%Y-%m-%d %H:%M:%S'))
+    starting_color = input("Computer Player? (w=white/b=black): ")
+    starting_FEN = input("Starting FEN position? (hit ENTER for standard starting position): ")
+
 main()
