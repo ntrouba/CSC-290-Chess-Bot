10ed892eb2b90f272c33e643f0853d2bb925fe38
Hillary Dunkley <hvgdunkley@gmail.com>
2024-10-22 16:10:16 -0400

    recent changes

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ def botMoves():
     return
 def main(): 
     board = chess.Board()
-    print(board)
+    #print(board)
     print("=====================================================\n              CS 290 Chess Bot Version 0.1\n=====================================================")
     print("Time: " , datetime.today().strftime('%Y-%m-%d %H:%M:%S'))
     starting_color = input("Computer Player? (w=white/b=black): ")
@@ -23,11 +23,11 @@ def print_board(board):
     print(board)
     print("FEN position:", board.fen())
 
-def user_input(board, player_color):
+def user_input(board, player_color, move):
      while True:
         move_input = input(f"{player_color.capitalize()}: ").strip()
-         try:
-                if move in board.legal_moves:
+        try:
+            if move in board.legal_moves:
                 return move
             else:
                 print("Invalid move. Please enter a legal move in UCI format.")
