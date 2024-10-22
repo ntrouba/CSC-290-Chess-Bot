dc2d02a292147ed99be6309a2b32c16adcacda06
Hillary Dunkley <hvgdunkley@gmail.com>
2024-10-22 16:42:46 -0400

    implemented user moves

--- main.py
+++ main.py
@@ -2,43 +2,50 @@ import chess
 import chess.engine
 import random
 from datetime import datetime
-def userMoves(board):
-    move = input("type move in UCI: ")
-    move = chess.Move.from_uci(move) 
-    board.push(move)
-    return board.push(move)
     
-def botMoves():
+def botMoves(board):
     capture_moves = [move for move in board.legal_moves if board.is_capture(move)]
     if capture_moves:
         return random.choice(capture_moves)
     else:
-    return random.choice(list(board.legal_moves))
+        return random.choice(list(board.legal_moves))
     
 def main(): 
     board = chess.Board()
     #print(board)
+    print(board.legal_moves)
+    
     print("=====================================================\n              CS 290 Chess Bot Version 0.1\n=====================================================")
     print("Time: " , datetime.today().strftime('%Y-%m-%d %H:%M:%S'))
-    starting_color = input("Computer Player? (w=white/b=black): ")
+    player_color = input("Computer Player? (w=white/b=black): ")
     starting_FEN = input("Starting FEN position? (hit ENTER for standard starting position): ")
-    userMoves(board)
+    while True:
+        user_input(board, player_color)
+        print(board.turn)
+        if board.turn != player_color: 
+            print(board.turn)
+
 
 def print_board(board):
     print("Current Board:")
     print(board)
     print("FEN position:", board.fen())
-
-# def user_input(board, player_color, move):
-#      while True:
-#        move_input = input(f"{player_color.capitalize()}: ").strip()
-#        try:
-#             if move in board.legal_moves:
-#                 return move
-#             else:
-#                 print("Invalid move. Please enter a legal move in UCI format.")
-#         except ValueError:
-#             print("Invalid input format. Please enter move in UCI format (e.g., e2e4).")
+def update_board(board, move): 
+    print(board)
+    board.push_san(move.uci())
+    print(board)
+    return 
+def user_input(board, player_color):
+        move_input = input(f"{player_color.capitalize()}: ").strip()
+        move = chess.Move.from_uci(move_input) 
+        try:
+            if move in board.legal_moves:
+                update_board(board, move)
+                return move
+            else:
+                print("Invalid move. Please enter a legal move in UCI format.")
+        except ValueError:
+            print("Invalid input format. Please enter move in UCI format (e.g., e2e4).")
 
     
 
